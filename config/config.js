2d68d1321a2c375dcfb83ab6adbc4b01cb049b20
NUKE-PC\Samuel Oriaku <oriaku.samuel@gmail.com>
2017-02-24 13:56:41 +0100

    First Commit

---
+++ config/config.js
@@ -0,0 +1,34 @@
+var path = require('path'),
+    rootPath = path.normalize(__dirname + '/..'),
+    env = process.env.NODE_ENV || 'development';
+
+var config = {
+  development: {
+    root: rootPath,
+    app: {
+      name: 'todo-do'
+    },
+    port: process.env.PORT || 3000,
+    db: 'mongodb://localhost/todo-do-development'
+  },
+
+  test: {
+    root: rootPath,
+    app: {
+      name: 'todo-do'
+    },
+    port: process.env.PORT || 3000,
+    db: 'mongodb://localhost/todo-do-test'
+  },
+
+  production: {
+    root: rootPath,
+    app: {
+      name: 'todo-do'
+    },
+    port: process.env.PORT || 3000,
+    db: 'mongodb://localhost/todo-do-production'
+  }
+};
+
+module.exports = config[env];
